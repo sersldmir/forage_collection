dda6d66271a94ed64a0a04c4ce0ee8670a80d6c3
sersldmir <2sergmir2@gmail.com>
2023-07-13 18:34:30 +0300

    reworked backend + finished app

--- app/src/main/java/citi_project/ApiRequester.java
+++ app/src/main/java/citi_project/ApiRequester.java
@@ -1,68 +1,64 @@
 package citi_project;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
 import java.net.URI;
 import java.net.http.*;
-import java.sql.Timestamp;
 import java.util.regex.*;
 
 public class ApiRequester {
 
-    private String stockName;
-    private HashMap<Timestamp, BigDecimal> info;
+    private String identifier;
 
-    public ApiRequester(String nameStock){
-        this.stockName = nameStock;
-        this.info = new HashMap<>();
+    public ApiRequester(String identifier){
+        this.identifier = identifier;
     }
 
-    public String getStockName() {
-        return stockName;
-    }
+    public double getStockRealPrice() throws IOException, InterruptedException{
 
-    public void setStockName(String stockName) {
-        this.stockName = stockName;
-    }
+        double price;
 
-    public void getStockPrice(int seconds) throws IOException, InterruptedException{
+        // request data
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://latest-stock-price.p.rapidapi.com/price?Indices=NIFTY%2050&Identifier=TITANEQN"))
+            .uri(URI.create("https://latest-stock-price.p.rapidapi.com/price?Indices=NIFTY%2050&Identifier="+this.identifier))
             .header("X-RapidAPI-Key", "65c155fd51msh901b00387edc8d4p143062jsnb71408090645")
             .header("X-RapidAPI-Host", "latest-stock-price.p.rapidapi.com")
             .method("GET", HttpRequest.BodyPublishers.noBody())
             .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println("-------------");
+        // System.out.println("-------------");
         String raw = response.body().toString();
-        System.out.println(raw);
+        // System.out.println(raw);
 
         // find price
-        String pricePattern = "(?<=open\":)\\d+(?=,)";
+        String pricePattern = "(?<=lastPrice\\\":)\\d+(?:\\.\\d+)?(?=,)";
         Pattern priceRegex = Pattern.compile(pricePattern);
         Matcher priceMatch = priceRegex.matcher(raw);
-        BigDecimal price;
         if (priceMatch.find()) {
             String number = priceMatch.group();
-            System.out.println("Number found: " + number);
-            price = new BigDecimal(priceMatch.group());
+            // System.out.println("Number found: " + number);
+            price = Double.parseDouble(number);
         } else {
-            System.out.println("No number found.");
-            price = new BigDecimal("99.99");
+            // System.out.println("No number found.");
+            price = Double.parseDouble("99.99");
         }
 
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        this.info.put(timestamp, price);
-        System.out.println("Sleeping");
-        TimeUnit.SECONDS.sleep(seconds);
-        System.out.println("Awake");
-        for (Timestamp key: this.info.keySet()){
-            System.out.println(key.toString());
-            System.out.println(this.info.get(key).toString());
-        }
+        return price;
     }
     
-    
+}
+
+class Main{
+    public static void main(String[] args)  throws IOException, InterruptedException{
+        ApiRequester api = new ApiRequester("TITANEQN");
+        System.out.println("TITANEQN");
+        System.out.println(api.getStockRealPrice());
+
+        ApiRequester api_ = new ApiRequester("LTEQN");
+        System.out.println("LTEQN");
+        System.out.println(api_.getStockRealPrice());
+
+        ApiRequester api__ = new ApiRequester("LTIMEQN");
+        System.out.println("LTIMEQN");
+        System.out.println(api__.getStockRealPrice());
+    }
 }

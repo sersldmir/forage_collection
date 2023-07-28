59c09fa4476989aebd26976438039007ef35b3cb
sersldmir <2sergmir2@gmail.com>
2023-07-28 15:07:47 +0300

    Implemented a simple region filter

--- simple_dash.py
+++ simple_dash.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, callback
 import plotly.express as px
 import pandas as pd
 
@@ -6,7 +6,7 @@ app = Dash(__name__)
 
 df = pd.read_csv("data/sales_data.csv", parse_dates=["date"])
 
-fig = px.line(data_frame=df, x="date", y="sales", color="region")
+# fig = px.line(data_frame=df, x="date", y="sales", color="region")
 
 app.layout = html.Div(children=[
     html.H1(children='My mini Dash web-dashboard'),
@@ -15,11 +15,29 @@ app.layout = html.Div(children=[
         Sales plot.
     '''),
 
+    dcc.Checklist(options=df["region"].unique(), value=df["region"].unique(), id="check-list"),
+
+
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='line-chart',
     )
 ])
+
+
+@callback(
+    Output("line-chart", "figure"),
+    Input("check-list", "value")
+)
+def update_line_chart(selected_regions):
+    
+    filtered_df = df[df["region"].isin(selected_regions)]
+
+    fig = px.line(data_frame=filtered_df, x="date", y="sales", color="region")
+
+    fig.update_layout(transition_duration=100)
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run(debug=True)
